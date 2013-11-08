0cc21530f11ae4ea1f8a24c9f553aabab8affb78
Tobias Hansson <tobias@tobiashansson.nu>
2013-11-08 21:58:10 +0100

    trying out some mongodb stuff

--- web.js
+++ web.js
@@ -1,9 +1,30 @@
 var express = require("express");
 var app = express();
+var MongoClient = require('mongodb').MongoClient, 
+  format = require('util').format;  
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL;
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+  MongoClient.connect(mongoUri, function(err, db) {
+    if(err) throw err;
+    var response = "";
+    var collection = db.collection('test_insert');
+    collection.insert({a:2}, function(err, docs) {
+                 
+      collection.count(function(err, count) {
+       response = format("count = %s", count);
+      });
+                        
+      // Locate all the entries using find
+      collection.find().toArray(function(err, results) {
+        // Let's close the db
+        db.close();
+        response.send(response)
+      });      
+    });
+  })  
+);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
