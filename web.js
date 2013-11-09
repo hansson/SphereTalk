7a41bd1537faa2d3f80c530b294c675eb5dc5d86
Tobias Hansson <tobias@tobiashansson.nu>
2013-11-09 01:28:21 +0100

    Trying if mongodb connection is working on heroku

--- web.js
+++ web.js
@@ -1,29 +1,26 @@
 var express = require("express");
 var app = express();
-var MongoClient = require('mongodb').MongoClient, 
-  format = require('util').format;  
+var mongoose = require('mongoose');
+var models = require('./models');
 
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL;
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/spheretalk';
 
-app.get('/', function(request, response) {
-  MongoClient.connect(mongoUri, function(err, db) {
-    if(err) throw err;
-    var response = "";
-    var collection = db.collection('test_insert');
-    collection.insert({a:2}, function(err, docs) {
-                 
-      collection.count(function(err, count) {
-       response = format("count = %s", count);
-      });
-                        
-      // Locate all the entries using find
-      collection.find().toArray(function(err, results) {
-        // Let's close the db
-        db.close();
-        response.send(response)
-      });      
-    });
-  });  
+mongoose.connect(mongoUri);
+
+app.get('/create', function(request, response) {
+  var newUser = new models.User({
+    username: 'test',
+    geo_location: 'a location'
+  });
+  newUser.save();
+  response.send("ok");
+});
+
+app.get('/find', function(request, response) {
+  models.User.findOne({username: "test"}, function(err,user) {
+    if(err);
+    response.send(user.geo_location);
+  });
 });
 
 var port = process.env.PORT || 5000;
